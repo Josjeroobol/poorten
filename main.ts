14a94c1bbb5a36d419471648c135003b8e926401
Josjeroobol <124696948+Josjeroobol@users.noreply.github.com>
2023-03-14 16:05:22 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) && !(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right))) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.clearScreen()
+    } else {
+        basic.showString("0")
+    }
+})
